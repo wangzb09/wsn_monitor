6a74ea22db252e3376bedcb0ab2265fa4b9317aa
wangzb09 <wangzhibo09@gmail.com>
2014-05-06 19:15:16 +0800

    http cmd

--- http.cpp
+++ http.cpp
@@ -16,7 +16,15 @@ using namespace std;
 #define dbgprint(x)
 #endif  
 
- char hello[]="HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: 20\r\n\r\nHello !中文Are you fine?\n";
+char hello[]="HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: 20\r\n\r\nHello !中文Are you fine?\n";
+
+//the command of http protocol
+#define HTTP_NOCMD	0
+#define HTTP_GET	1
+#define HTTP_POST	2
+
+#define URL_LEN		1024
+#define BUFFLEN		4096
 
 class Http
 {
@@ -25,23 +33,110 @@ public:
 	void PrintAddr();
 	void RecvHttp();
 	int Input(char msg[],int len);
+	int Response();
 
 private:
 	int socket;
 	sockaddr_in client_addr;
+	int cmd;
+	char url[URL_LEN];
 
 };
 
-Http::Http(int sock,sockaddr_in clientaddr):socket(sock),client_addr(clientaddr)
+Http::Http(int sock,sockaddr_in clientaddr):socket(sock),client_addr(clientaddr),cmd(HTTP_NOCMD)
 {
 }
 
 int Http::Input(char msg[],int len)
 {
-	char buff[5000];
-	strncpy(buff,msg,len);
-	buff[len]=0;
-	printf("MSG[%s]\n",buff);
+	int i;
+	int start=0;	//scan http head
+	for(i=0;i<len-1;i++)
+	{
+		if(msg[i]=='\r' && msg[i+1]=='\n')
+		{
+			int len=i-start;
+			char buff1[URL_LEN]="";
+			char buff2[URL_LEN]="";
+			char buff3[URL_LEN]="";
+			char buff4[URL_LEN]="";
+			int argc;
+			msg[i]=0;
+			if((argc=sscanf(msg+start,"%s%s%s%s",buff1,buff2,buff3,buff4)) >0)	//get command
+			{
+				dbgprint("CMD agrc=%d\n[1]%s[2]%s[3]%s[4]%s[]\n",argc,buff1,buff2,buff3,buff4);
+				if(strcmp(buff1,"GET")==0)
+				{
+					if(argc>=2)
+					{
+						cmd=HTTP_GET;	//the is the get command
+						strcpy(url,buff2);	//the url to get
+					}
+					else return 1;
+				}
+				
+				
+				
+			}
+			start=i+2;
+			i++;
+		}
+	}
+	return 0;
+}
+int Http::Response()
+{
+	switch(cmd)
+	{
+		case HTTP_GET:
+		{
+			if(!strcmp(url,"/") || !strcmp(url,"/index.htm") || !strcmp(url,"/index.html"))
+			{	//return the default web page
+				FILE *fp=fopen("webs/index.htm","rb");
+				if(!fp)
+				{
+					dbgprint("open webs/index.htm fail\n");
+					return 3;
+				}
+				fseek(fp,0,SEEK_END);
+				int flen=ftell(fp);
+				fseek(fp,0,SEEK_SET);
+				char buff[BUFFLEN];
+				sprintf(buff,"HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: %d\r\n\r\n",flen);
+				if(send(socket,buff,strlen(buff),0)==-1)
+				{
+					printf("Send Error\n");
+					pthread_exit((void *)1);
+				}
+				int size;
+				while((size=fread(buff,sizeof(char),BUFFLEN,fp))>0)
+				{
+					if(send(socket,buff,size,0)==-1)
+					{
+						printf("Send Error\n");
+						pthread_exit((void *)1);
+					}				
+				}
+				fclose(fp);
+				return 0;				
+			}
+			break;
+		}
+		case HTTP_POST:
+		{
+			break;
+		}
+		case HTTP_NOCMD:
+		{
+			printf("No command in HTTP head\n");
+			return 1;
+		}
+		default:
+		{
+			printf("Unknown http command\n");
+			return 2;
+		}
+	}
 	return 0;
 }
 
@@ -55,7 +150,7 @@ void Http::PrintAddr()
 	}
 }
 
-#define BUFFLEN 4096
+
 void Http::RecvHttp()
 {
 	dbgprint("Get connection from [%s:%d]\n",inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
@@ -67,22 +162,22 @@ void Http::RecvHttp()
 	bool flag=true;
 	while(flag)
 	{
-		len=recv(socket, buff+start_buff, BUFFLEN-start_buff, 0);
+		len=recv(socket, buff+start_buff, BUFFLEN-start_buff, 0);		//receive data from client
 		if(len>0)
 		{
 			for(i=start_search;i<start_buff+len-3;i++)
 			{
-				if(buff[i]=='\r' && buff[i+2]=='\r' && buff[i+1]=='\n' && buff[i+3]=='\n')
+				if(buff[i]=='\r' && buff[i+2]=='\r' && buff[i+1]=='\n' && buff[i+3]=='\n')	//the http head is finished
 				{
-					headlen=i;
+					headlen=i+2;	//the length of http head(There is only 1 '\r\n' at the end)
 					start_search=i+4;
 					start_buff=start_buff+len;
 					flag=false;
 					break;
 				}
 			}
-			if(flag)
-			{
+			if(flag)	//the http head is not finished
+			{	//update the buffer information
 				start_buff+=len;
 				start_search=i;	
 				if(start_buff>=BUFFLEN)
@@ -103,8 +198,20 @@ void Http::RecvHttp()
 		}
 	}
 	buff[headlen]=0;
-	printf("[MSG]\n%s\n",buff);
-	PrintAddr();
+	dbgprint("[MSG]\n%s\n",buff);
+	if(Input(buff,headlen)!=0)
+	{
+		printf("HTTP HEAD ERROR, Disconnect with [%s:%d]\n",inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
+		close(socket);
+		return;
+	}
+	if(Response()!=0)		//response to the client according to the command
+	{
+		printf("Response failed to [%s:%d]\n",inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
+		close(socket);
+		return;
+	}
+	//PrintAddr();
 	dbgprint("Disconnect with [%s:%d]\n",inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
 	close(socket);
 	
